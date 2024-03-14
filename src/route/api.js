816de62fbdbe458d17a28b7ee03221d0bba921e0
sabir-phantom <sabir.sbair@gmail.com>
2024-03-14 19:25:55 +0600

    creating route

--- src/route/api.js
+++ src/route/api.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
+const {   createProfile, userLogin, selectProfile, profileUpdate } = require('../controller/userController');
+const AuthMiddleware = require('../middleware/AuthMiddleware');
+const { createTodo, selectTodo, updateTodo, statusUpdateTodo, deleteTodo } = require('../controller/TodoController');
 
 
 
 const router = express.Router();
+
+
+
 
 
 
